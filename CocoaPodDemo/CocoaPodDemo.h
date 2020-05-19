36b96580fd278abdc11b957c366f5ec4b00884e7
engleliu <engleliu@tencent.com>
2020-05-19 21:16:35 +0800

    Initial Commit

---
+++ CocoaPodDemo/CocoaPodDemo.h
@@ -0,0 +1,19 @@
+//
+//  CocoaPodDemo.h
+//  CocoaPodDemo
+//
+//  Created by 白牙 on 2020/5/19.
+//  Copyright © 2020 白牙. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoaPodDemo.
+FOUNDATION_EXPORT double CocoaPodDemoVersionNumber;
+
+//! Project version string for CocoaPodDemo.
+FOUNDATION_EXPORT const unsigned char CocoaPodDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaPodDemo/PublicHeader.h>
+
+
---
+++ CocoaPodDemoTests/CocoaPodDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  CocoaPodDemoTests.m
+//  CocoaPodDemoTests
+//
+//  Created by 白牙 on 2020/5/19.
+//  Copyright © 2020 白牙. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CocoaPodDemoTests : XCTestCase
+
+@end
+
+@implementation CocoaPodDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

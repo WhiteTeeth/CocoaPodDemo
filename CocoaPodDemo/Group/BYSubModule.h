258eafb06c824475c8e2dadd372836aa7deb3740
engleliu <engleliu@tencent.com>
2020-05-20 15:32:23 +0800

    add sub module

---
+++ CocoaPodDemo/Group/BYSubModule.h
@@ -0,0 +1,19 @@
+//
+//  BYSubModule.h
+//  CocoaPodDemo
+//
+//  Created by 白牙 on 2020/5/20.
+//  Copyright © 2020 白牙. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BYSubModule : NSObject
+
++ (void)printHello;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CocoaPodDemo/Group/BYSubModule.m
@@ -0,0 +1,17 @@
+//
+//  BYSubModule.m
+//  CocoaPodDemo
+//
+//  Created by 白牙 on 2020/5/20.
+//  Copyright © 2020 白牙. All rights reserved.
+//
+
+#import "BYSubModule.h"
+
+@implementation BYSubModule
+
++ (void)printHello {
+    NSLog(@"BYSubModule");
+}
+
+@end
